701e37e824095705038db92638e8b1c51250f2a9
Jkapper <jk24199@live.missouristate.edu>
2017-04-04 22:55:30 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Source Code 
-function validateForm() {
-    var x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-        alert("Name must be filled out");
-        return false;
-    }
-}
